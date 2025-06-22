ac05ba415e561283aa7ea5e4d73df5e6c2ed8dd4
feige996 <1020102647@qq.com>
2025-06-22 11:07:35 +0800

    refactor: 替换 sard-uniapp 为 uview-plus 组件库

--- pages.config.ts
+++ pages.config.ts
@@ -13,7 +13,11 @@ export default defineUniPages({
     autoscan: true,
     custom: {
       '^fg-(.*)': '@/components/fg-$1/fg-$1.vue',
-      '^sar-(.*)': 'sard-uniapp/components/$1/$1.vue',
+      // 下面的是 uview-plus 的配置
+      '^u--(.*)': 'uview-plus/components/u-$1/u-$1.vue',
+      '^up-(.*)': 'uview-plus/components/u-$1/u-$1.vue',
+      '^u-([^-].*)': 'uview-plus/components/u-$1/u-$1.vue',
+      // 下面是z-paging 的配置
       '^(?!z-paging-refresh|z-paging-load-more)z-paging(.*)':
         'z-paging/components/z-paging$1/z-paging$1.vue',
     },
