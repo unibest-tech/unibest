4a3e06fbcde77f29875690135cf56fec212a8333
feige996 <1020102647@qq.com>
2025-07-12 13:21:17 +0800

    chore: 与main分支同步

--- src/pages/about/about.vue
+++ src/pages/about/about.vue
@@ -11,12 +11,6 @@
 import RequestComp from './components/request.vue'
 import UploadComp from './components/upload.vue'
 
-function gotoI18nPage() {
-  uni.navigateTo({
-    url: '/pages/about/i18n',
-  })
-}
-
 // 奇怪：同样的代码放在 vue 里面不会校验到错误，放在 .ts 文件里面会校验到错误
 // const testOxlint = (name: string) => {
 //   console.log('oxlint')
@@ -41,11 +35,6 @@ function gotoAlova() {
     </view>
     <RequestComp />
     <UploadComp />
-    <view class="text-center">
-      <wd-button @click="gotoI18nPage()">
-        去I18n页面
-      </wd-button>
-    </view>
     <button class="w-200px text-green" @click="gotoAlova">
       前往 alova 页面
     </button>
@@ -54,7 +43,10 @@ function gotoAlova() {
 
 <style lang="scss" scoped>
 .test-css {
+  // 16rpx=>0.5rem
+  padding-bottom: 16rpx;
   // mt-4=>1rem=>16px;
   margin-top: 16px;
+  text-align: center;
 }
 </style>
