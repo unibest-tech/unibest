74424e1b902d3ca5fbca33c4329ddb50842715e2
feige996 <1020102647@qq.com>
2025-08-01 18:07:19 +0800

    feat: 启用自定义tabBar并更新UI组件引用
    将tabBar配置改为自定义模式，同时将wd-button组件替换为tm-button组件，并更新UI组件官网链接

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -77,13 +77,13 @@ console.log('index')
       </text>
     </view>
     <view class="mt-4 text-center">
-      <wd-button type="primary">
+      <tm-button>
         UI组件按钮
-      </wd-button>
+      </tm-button>
     </view>
     <view class="mt-4 text-center">
       UI组件官网：<text class="text-green-500">
-        https://wot-design-uni.cn
+        https://tmui.design (3.2.x)
       </text>
     </view>
   </view>
