4f9ed57f8c3b3c9349c77da43d1fa69ba1ddc942
feige996 <1020102647@qq.com>
2025-08-01 17:43:03 +0800

    style(theme): 将主题色primary从CSS变量改为固定值

--- uno.config.ts
+++ uno.config.ts
@@ -56,7 +56,7 @@ export default defineConfig({
   theme: {
     colors: {
       /** 主题色，用法如: text-primary */
-      primary: 'var(--wot-color-theme,#0957DE)',
+      primary: '#0957DE',
     },
     fontSize: {
       /** 提供更小号的字体，用法如：text-2xs */
