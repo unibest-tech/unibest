40e039c10f3283490a52cdacc0aa5c2ffb67d001
feige996 <1020102647@qq.com>
2025-08-05 18:01:13 +0800

    refactor(tabbar): 将wd-icon组件替换为uv-icon组件

--- src/tabbar/index.vue
+++ src/tabbar/index.vue
@@ -67,7 +67,7 @@ function getImageByIndex(index: number, item: { iconActive: string, icon: string
             <!-- 如：<wd-icon name="home" /> (https://wot-design-uni.cn/component/icon.html) -->
             <!-- 如：<uv-icon name="home" /> (https://www.uvui.cn/components/icon.html) -->
             <!-- 如：<sar-icon name="image" /> (https://sard.wzt.zone/sard-uniapp-docs/components/icon)(sar没有home图标^_^) -->
-            <wd-icon :name="item.icon" size="20" />
+            <uv-icon :name="item.icon" size="20" />
           </template>
           <template v-if="item.iconType === 'unocss' || item.iconType === 'iconfont'">
             <view :class="item.icon" class="text-20px" />
