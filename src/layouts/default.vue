490d244976920fda246667b26d6799bf526a9380
feige996 <1020102647@qq.com>
2025-12-31 16:28:51 +0800

    refactor(layout): 移除默认布局中的冗余脚本代码

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,15 +1,3 @@
-<script lang="ts" setup>
-import { getI18nText } from '@/tabbar/i18n'
-import { getCurrentPageI18nKey } from '@/utils'
-
-onShow(() => {
-  console.log('layout default - onShow')
-  uni.setNavigationBarTitle({
-    title: getI18nText(getCurrentPageI18nKey()),
-  })
-})
-</script>
-
 <template>
   <slot />
 </template>
